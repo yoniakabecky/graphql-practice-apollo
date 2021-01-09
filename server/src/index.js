f4111e9dc3a04e4a162a540bd62bd2d30863f87d
Yoni <yoniakabecky@gmail.com>
2021-01-08 17:25:17 -0800

    Build a schema

--- server/src/index.js
+++ server/src/index.js
@@ -4,3 +4,10 @@ const typeDefs = require("./schema");
 
 const server = new ApolloServer({ typeDefs });
 
+server.listen().then(() => {
+  console.log(`
+    Server is running!
+    Listening on port 4000
+    Explore at https://studio.apollographql.com/dev
+  `);
+});
